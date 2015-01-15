0e032d079997bd4f5e4c0fa61e1c7d48ce25e6fb
dream <dxs9642@gmail.com>
2015-01-15 15:39:55 +0800

    Add Button Image, LaunchImage

--- DreamWeibo/AppDelegate.swift
+++ DreamWeibo/AppDelegate.swift
@@ -16,6 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow()
+        window?.frame = UIScreen.mainScreen().bounds
+        
+        var tabbar = UITabBarController()
+        self.window?.rootViewController = tabbar
+        
+        window?.makeKeyAndVisible()
         return true
     }
 
